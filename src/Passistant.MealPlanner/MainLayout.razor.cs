c4a84849edce8b3af8624e90dc6ed80fae852258
Saji Weerasingham <saji.weerasingham@outlook.com>
2026-01-22 07:42:40 +1000

    MudBlazor Dark Theme

---
+++ src/Passistant.MealPlanner/MainLayout.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Passistant.MealPlanner;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    private MudThemeProvider ThemeProvider { get; set; } = null!;
+
+    private bool IsDarkMode { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            this.IsDarkMode = await this.ThemeProvider.GetSystemDarkModeAsync();
+            await this.ThemeProvider.WatchSystemDarkModeAsync(this.OnSystemPreferenceChangedAsync);
+            this.StateHasChanged();
+        }
+    }
+
+    private Task OnSystemPreferenceChangedAsync(bool isDarkMode)
+    {
+        this.IsDarkMode = isDarkMode;
+        this.StateHasChanged();
+        return Task.CompletedTask;
+    }
+}
